3a06bab708dbd3bca07406a98363fb0ed13c16a7
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2021-05-11 16:43:55 +0300

    button

--- packages/button/src/index.vue
+++ packages/button/src/index.vue
@@ -49,12 +49,6 @@ export default {
 			default: false
 		},
 
-		esatType: {
-			type: Boolean,
-			required: false,
-			default: false
-		},
-
 		iconOnly: {
 			type: Boolean,
 			required: false,
