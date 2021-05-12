5eb526354ee21f6fba5b5f191f83c54063175f2f
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2021-05-12 15:25:40 +0300

    ant design test

--- packages/button/src/index.vue
+++ packages/button/src/index.vue
@@ -1,19 +1,25 @@
 <template>
-  <button style="min-width: 200px; min-height: 300px;">
-		<slot></slot>
-	</button>
+  <a-button style="min-width: 200px; min-height: 300px">
+    <slot></slot>
+  </a-button>
+  <a-button type="primary" ghost>Primary</a-button>
+    <a-button ghost>Default</a-button>
+    <a-button type="dashed" ghost>Dashed</a-button>
+    <a-button type="danger" ghost>Danger</a-button>
+    <a-button type="link" ghost>Link</a-button>
 </template>
 <script>
-//import Button from "ant-design-vue/lib/button";
+import Button from "ant-design-vue/lib/button";
 export default {
   name: "MhButton",
-  setup() {
+  components: {
+    [Button.name]: Button,
   },
-  created(){
-    console.log('it works! 4');
-  }
-  // components: {
-  //   [Button.name]: Button
-  // }
+  methods: {
+    click() {
+      console.log('click!');
+      this.$emit("click");
+    },
+  },
 };
 </script>
