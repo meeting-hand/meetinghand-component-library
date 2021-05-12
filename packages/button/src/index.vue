d41bbba2c861ac93006ec5ef6331149410d1bb4f
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2021-05-12 10:32:27 +0300

    package test

--- packages/button/src/index.vue
+++ packages/button/src/index.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>
-        <h1>Rick Sanchez</h1>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick Sanchez">
-    </div>
+  <button>
+		<slot></slot>
+	</button>
 </template>
 <script>
 //import Button from "ant-design-vue/lib/button";
 export default {
   name: "MhButton",
+  setup() {
+  },
   // components: {
   //   [Button.name]: Button
   // }
