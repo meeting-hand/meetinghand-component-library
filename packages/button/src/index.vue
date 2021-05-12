29027c2d6ae2d6a0d0d573f4a9aa87a1e090be50
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2021-05-12 10:21:04 +0300

    button test

--- packages/button/src/index.vue
+++ packages/button/src/index.vue
@@ -1,12 +1,13 @@
 <template>
-  <a-button type="primary"><slot></slot></a-button>
+  <!-- <a-button type="primary"><slot></slot></a-button> -->
+  <button><slot></slot></button>
 </template>
 <script>
-import Button from "ant-design-vue/lib/button";
+//import Button from "ant-design-vue/lib/button";
 export default {
   name: "MhButton",
-  components: {
-    [Button.name]: Button
-  }
+  // components: {
+  //   [Button.name]: Button
+  // }
 };
 </script>
