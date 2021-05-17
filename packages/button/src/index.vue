9a7b714d82c9af27a9033a8bb30780b3a4a832d1
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2021-05-17 10:19:22 +0300

    fix

--- packages/button/src/index.vue
+++ packages/button/src/index.vue
@@ -1,15 +1,10 @@
 <template>
-  <a-button style="min-width: 200px; min-height: 300px">
+  <a-button style="min-width: 400px; min-height: 300px">
     <slot></slot>
   </a-button>
-  <a-button type="primary" ghost>Primary</a-button>
-    <a-button ghost>Default</a-button>
-    <a-button type="dashed" ghost>Dashed</a-button>
-    <a-button type="danger" ghost>Danger</a-button>
-    <a-button type="link" ghost>Link</a-button>
 </template>
 <script>
-import { Button } from "ant-design-vue/lib/button";
+import Button from "ant-design-vue/lib/button";
 export default {
   name: "MhButton",
   components: {
