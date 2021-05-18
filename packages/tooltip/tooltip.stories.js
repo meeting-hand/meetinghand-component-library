3358eda70a9bf1c91374a698b5c1634fd120c90f
EmreKaanSurgun <emrekaansurgun@gmail.com>
2021-05-18 16:58:20 +0300

    tooltip.css added

--- packages/tooltip/tooltip.stories.js
+++ packages/tooltip/tooltip.stories.js
@@ -1,11 +1,15 @@
 import { storiesOf } from "@storybook/vue3";
 import Tooltip from "./index";
 
-storiesOf("Tooltip", module).add("Tooltip!", () => ({
+import "../../assets/storybook/tooltip.css";
+
+
+storiesOf("Tooltip", module).add("All", () => ({
   components: { Tooltip },
   template: `
+    <div class="tooltip-wrapper">
       <h1>Placement Top</h1>
-      <tooltip size="large" placement="top" text="Lorem ipsum dolor sit amet, orbi vitae faucibus enim.">
+      <tooltip size="large" placement="top" text="Lorem ipsum dolor sit amet.">
       Tooltip will show when mouse enter.
       </tooltip>
       <h1>Placement Bottom</h1>
@@ -20,6 +24,7 @@ storiesOf("Tooltip", module).add("Tooltip!", () => ({
       <tooltip size="small" placement="left" text="Lorem ipsum dolor sit amet.">
       Tooltip will show when mouse enter.
       </tooltip>
+    <div/>
     `,
 }));
 
