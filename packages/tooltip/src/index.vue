f733c5c01410489868f821cd7052290ab524604f
EmreKaanSurgun <emrekaansurgun@gmail.com>
2021-05-18 16:50:25 +0300

    tooltip and divider added

--- packages/tooltip/src/index.vue
+++ packages/tooltip/src/index.vue
@@ -1,64 +1,32 @@
 <template>
-  <div :class="[`size-${size}`]">
-    <p :class="[`font-${size}`]">
-      Lorem ipsum dolor sit amet, orbi vitae faucibus enim.
-    </p>
-  </div>
+  <a-tooltip
+    color="transparent"
+    :overlayClassName="`mh-tooltip`"
+    :placement="`${placement}`"
+  >
+    <template #title>{{ text }}</template>
+    <slot></slot>
+  </a-tooltip>
 </template>
 
 <script>
+import Tooltip from "ant-design-vue/lib/tooltip";
+
 export default {
   name: "Tooltip",
+  components: {
+    [Tooltip.name]: Tooltip,
+  },
   props: {
-    size: {
+    placement: {
       type: String,
-      default: "large",
-      validator: (_v) => ["large", "small"].includes(_v),
+      default: "top",
+      validator: (_v) => ["top", "left", "right", "bottom"].includes(_v),
+    },
+    text: {
+      type: String,
+      required: true,
     },
   },
 };
 </script>
-
-<style scoped>
-div {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 16px;
-  position: relative;
-  /* Mono/Light-Grey-5 */
-  border: 1.5px solid #dfdfdf;
-  box-sizing: border-box;
-  /* Box-Shadow-2 */
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.08);
-  border-radius: 4px;
-}
-.size-large {
-  width: 224px;
-  height: 64px;
-}
-.size-small {
-  width: 190px;
-  height: 48px;
-}
-p {
-  position: static;
-  left: 16px;
-  top: 16px;
-  font-family: Inter;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 12px;
-  line-height: 16px;
-  /* Mono/Grey-3 */
-  color: #565656;
-}
-.font-large {
-  width: 192px;
-  height: 32px;
-}
-.font-small {
-  width: 158px;
-  height: 16px;
-}
-</style>
