44423867df9a899146c526a1e938272a6c2881c4
EmreKaanSurgun <emrekaansurgun@gmail.com>
2021-05-19 12:57:27 +0300

    added icons

---
+++ packages/style/icons/orderUp.vue
@@ -0,0 +1,46 @@
+<template>
+  <span>
+    <svg
+      width="8"
+      height="5"
+      viewBox="0 0 8 5"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7 4L4 1L0.999999 4"
+        stroke="#565656"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+
+    <svg
+      width="8"
+      height="5"
+      viewBox="0 0 8 5"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7 1L4 4L1 1"
+        stroke="#B6B6B6"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  </span>
+</template>
+
+<style lang="scss" scoped>
+span {
+  svg {
+    &:first-of-type {
+      margin-bottom: 3px;
+    }
+    display: block;
+  }
+}
+</style>
--- packages/style/style.stories.js
+++ packages/style/style.stories.js
@@ -1,5 +1,9 @@
 import { storiesOf } from "@storybook/vue3";
 
+import MhIcon from "./icons/index";
+
+import "../../assets/storybook/style.css";
+
 storiesOf("Style", module).add("Style!", () => ({
   template: `
     <h1>MeetingHand Base Style Library</h1>
@@ -7,6 +11,22 @@ storiesOf("Style", module).add("Style!", () => ({
 
     <h2>Colors</h2>
     <p>assets/colors.scss</p>
+    `,
+}));
+
+storiesOf("Style", module).add("icons", () => ({
+  components: { MhIcon },
+  template: `
+    <h1>Icons</h1>
+    <mh-icon name="arrow-down" />
+    <mh-icon name="arrow-up" />
+    <mh-icon name="arrow-left" />
+    <mh-icon name="arrow-right" />
+    <mh-icon name="order-down" />
+    <mh-icon name="order-up" />
+    <mh-icon name="sub-right-arrow" />
+    <mh-icon name="sub-left-arrow" />
+    <mh-icon name="wait" />
     `,
 }));
 
