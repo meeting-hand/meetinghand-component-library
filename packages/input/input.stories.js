94314c94fc3d7b7f9eced22d72795ad789e8fa20
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2021-05-20 11:47:42 +0300

    input icons

--- packages/input/input.stories.js
+++ packages/input/input.stories.js
@@ -8,10 +8,19 @@ storiesOf("Input", module).add("Input", () => ({
   template: `
       <h1>Standart</h1>
       <mh-input />
+      <br />
+      <br />
+      <mh-input left-icon="arrow-down" right-icon="arrow-up" />
       <hr />
       <h1>Error</h1>
       <mh-input has-error error-message="this field is required" />
+      <br />
+      <br />
+      <mh-input has-error error-message="this field is required" left-icon="arrow-down" right-icon="arrow-up" />
       <h1>Disabled</h1>
       <mh-input disabled />
+      <br />
+      <br />
+      <mh-input disabled left-icon="arrow-down" right-icon="arrow-up" />
     `,
 }));
--- packages/input/src/index.vue
+++ packages/input/src/index.vue
@@ -3,7 +3,14 @@
     :placeholder="placeholder"
     :class="[{ error: hasError }]"
     :disabled="disabled"
-  />
+  >
+    <template #prefix v-if="leftIcon">
+      <mh-icon :name="leftIcon" />
+    </template>
+    <template #suffix v-if="rightIcon">
+      <mh-icon :name="rightIcon" />
+    </template>
+  </a-input>
   <span v-if="errorMessage" class="mh-input__error">
     {{ errorMessage }}
   </span>
@@ -11,6 +18,7 @@
 
 <script>
 import Input from "ant-design-vue/lib/input";
+import MhIcon from "@meetinghand/style/icons/index";
 export default {
   name: "MhInput",
   props: {
@@ -30,9 +38,18 @@ export default {
       type: String,
       default: "",
     },
+    leftIcon: {
+      type: String,
+      default: null,
+    },
+    rightIcon: {
+      type: String,
+      default: null,
+    },
   },
   components: {
     [Input.name]: Input,
+    MhIcon,
   },
 };
 </script>
