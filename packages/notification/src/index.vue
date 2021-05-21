6129aaacd6f1e1bd3e469cc05a283dec79857045
EmreKaanSurgun <emrekaansurgun@gmail.com>
2021-05-21 16:54:27 +0300

    notification component added

---
+++ packages/notification/src/index.vue
@@ -0,0 +1,93 @@
+<template></template>
+
+<script>
+import Notification from "ant-design-vue/lib/notification";
+import Button from "ant-design-vue/lib/button";
+import MHIcon from "@meetinghand/style/icons/index";
+import StatusSuccess from "@meetinghand/style/icons/systemStatusSuccess";
+import StatusError from "@meetinghand/style/icons/systemStatusError";
+import StatusInfo from "@meetinghand/style/icons/systemStatusInfo";
+import StatusHint from "@meetinghand/style/icons/systemStatusHint";
+import { h } from "vue";
+
+export default {
+  name: "MhNotification",
+  components: {
+    [Notification.name]: Notification,
+    [Button.name]: Button,
+    "mh-icon": MHIcon,
+    "status-success": StatusSuccess,
+  },
+  props: {
+    type: {
+      type: String,
+      default: "success",
+      validator: (_v) => ["success", "error", "info", "hint"].includes(_v),
+    },
+    description: {
+      type: String,
+      default: null,
+    },
+    title: {
+      type: String,
+      default: null,
+    },
+    icon: {
+      type: String,
+      default: null,
+      validator: (_v) =>
+        [
+          null,
+          "status-success",
+          "StatusError",
+          "StatusInfo",
+          "StatusHint",
+        ].includes(_v),
+    },
+    modelValue: {
+      type: Boolean,
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+    },
+    placement: {
+      type: String,
+      required: true,
+      default: "topRight",
+      validator: (_v) =>
+        ["topRight", "topLeft", "bottomRight", "bottomLeft"].includes(_v),
+    },
+  },
+
+  setup(props, { emit }) {
+    const openNotification = () => {
+      Notification.open({
+        message: `${props.title}`,
+        description: `${props.description}`,
+        duration: Number(`${props.duration}`),
+        class: `${props.type}`,
+        icon: h(StatusSuccess),
+        placement: `${props.placement}`,
+        onClose: () => {
+          emit("update:modelValue", false);
+        },
+      });
+    };
+
+    return {
+      openNotification,
+    };
+  },
+  created() {
+    this.openNotification();
+  },
+};
+
+// icon: h(`${props.icon}`),
+</script>
+
+<style scoped>
+</style>
+
