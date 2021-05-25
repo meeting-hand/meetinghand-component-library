06e05d37f8b06ec75f49ab5213eb75a7e02a7103
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2021-05-25 13:52:31 +0300

    datepicker v2

--- packages/date-picker/date-picker.stories.js
+++ packages/date-picker/date-picker.stories.js
@@ -7,14 +7,22 @@ storiesOf("DatePicker", module).add("DatePicker!", () => ({
 		return {
 			date1: "",
 			date2: "15.01.2021",
+			date3: null,
+			date4: "25.01.2021",
 		};
 	},
 	template: `
 			<h1>Default</h1>
-      <mh-date-picker v-model="date1" /> <span style="margin-left: 40px;">{{ date1 }}</span>
+      <mh-date-picker v-model="date1" placeholder="Default Empty Date" /> <span style="margin-left: 40px;">{{ date1 }}</span>
 			<br />
 			<br />
-      <mh-date-picker v-model="date2" /> <span style="margin-left: 40px;">{{ date2 }}</span>
+      <mh-date-picker v-model="date2" placeholder="Placeholder" /> <span style="margin-left: 40px;">{{ date2 }}</span>
+			<hr />
+			<h1>Error</h1>
+      <mh-date-picker v-model="date3" has-error error-message="Field is required" placeholder="Error Date Placeholder" />
+			<hr />
+			<h1>Disabled</h1>
+      <mh-date-picker v-model="date4" disabled placeholder="Disabled Date Placeholder" />
 			<hr />
     `,
 }));
--- packages/date-picker/src/index.vue
+++ packages/date-picker/src/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <a-date-picker v-model:value="value" :format="format" :valueFormat="format" />
+  <a-date-picker
+    v-model:value="value"
+    :format="format"
+    :valueFormat="format"
+    :class="[{ error: hasError }]"
+    :disabled="disabled"
+    :placeholder="placeholder"
+    :suffix-icon="icon"
+    :allowClear="false"
+  />
+  <span v-if="errorMessage" class="mh-input__error">
+    {{ errorMessage }}
+  </span>
 </template>
 
 <script>
 import DatePicker from "ant-design-vue/lib/date-picker";
+
+import MhDate from "@meetinghand/style/icons/uiDate";
+import { h } from "vue";
 
 export default {
   name: "MhDatePicker",
@@ -20,6 +35,22 @@ export default {
       default: "DD.MM.YYYY",
       validator: (_v) => ["DD.MM.YYYY", "MM.DD.YYYY"].includes(_v),
     },
+    hasError: {
+      type: Boolean,
+      default: false,
+    },
+    errorMessage: {
+      type: String,
+      default: null,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
   },
   computed: {
     value: {
@@ -30,6 +61,12 @@ export default {
         this.$emit("update:modelValue", value);
       },
     },
+  },
+  setup() {
+    const icon = h(MhDate);
+    return {
+      icon,
+    };
   },
 };
 </script>
