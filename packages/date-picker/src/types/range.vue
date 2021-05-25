f6abfb81e0243c0cc5844aa7450199e48bce3b60
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2021-05-25 18:55:28 +0300

    date range

--- packages/date-picker/src/types/range.vue
+++ packages/date-picker/src/types/range.vue
@@ -3,24 +3,31 @@
     v-model:value="value"
     :format="format"
     :valueFormat="format[0]"
-    :class="[{ error: hasError }]"
+    :class="[
+      { error: hasError },
+      'mh-range-picker',
+      { 'mh-range-picker-opened': status },
+    ]"
     :disabled="disabled"
     :placeholder="placeholder"
-    :suffix-icon="icon"
     :allowClear="false"
+    @openChange="openChange"
+    separator=""
   />
 </template>
 
 <script>
 import DatePicker from "ant-design-vue/lib/date-picker";
 
-import MhDate from "@meetinghand/style/icons/uiDate";
-import { h } from "vue";
-
 export default {
   name: "MhDatePickerRange",
   components: {
     [DatePicker.RangePicker.name]: DatePicker.RangePicker,
+  },
+  data() {
+    return {
+      status: false,
+    };
   },
   props: {
     modelValue: {
@@ -54,11 +61,10 @@ export default {
       },
     },
   },
-  setup() {
-    const icon = h(MhDate);
-    return {
-      icon,
-    };
+  methods: {
+    openChange(status) {
+      this.status = status;
+    },
   },
 };
 </script>
