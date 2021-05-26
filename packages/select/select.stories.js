181a5419bd7652a003962faefc9dec406c3fa4a1
EmreKaanSurgun <emrekaansurgun@gmail.com>
2021-05-26 15:50:22 +0300

    select component eddited

--- packages/select/select.stories.js
+++ packages/select/select.stories.js
@@ -34,5 +34,15 @@ storiesOf("Select", module).add("Select!", () => ({
 			<h1>Disabled</h1>
       <mh-select v-model="select3" :options="selectOpts" searchable placeholder="Birini seciniz" disabled /> <span v-if="select3" style="margin-left: 30px;">Value: {{ select3 }}</span>
 			<hr />
+			<h1>Error</h1>
+      <mh-select v-model="select1" :options="selectOpts" placeholder="Birini seciniz" has-error error-message="this field is required"/> <span v-if="select1" style="margin-left: 30px;">Value: {{ select1 }}</span>
+			<hr />
+			<h1>Small</h1>
+      		<mh-select v-model="select1" :options="selectOpts" placeholder="Birini seciniz" size="small"/> <span v-if="select1" style="margin-left: 30px;">Value: {{ select1 }}</span>
+			<hr />
+      		<mh-select v-model="select2" disabled :options="selectOpts" placeholder="Birini seciniz" size="small"/> <span v-if="select2" style="margin-left: 30px;">Value: {{ select2 }}</span>
+			<hr />
+      		<mh-select v-model="select3" has-error error-message="this field is required" :options="selectOpts" placeholder="Birini seciniz" size="small"/> <span v-if="select3" style="margin-left: 30px;">Value: {{ select3 }}</span>
+			<hr />
     `,
 }));
