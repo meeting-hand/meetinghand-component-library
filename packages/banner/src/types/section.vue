7e74e053e367b828efa3f111477beb4270ba71d0
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2021-05-27 10:26:23 +0300

    banner refactor

--- packages/banner/src/types/section.vue
+++ packages/banner/src/types/section.vue
@@ -4,6 +4,7 @@
     type="info"
     show-icon
     v-if="`${button}` === 'null'"
+    :icon="infoIcon"
   />
   <!-- Event trigger will be added -->
   <a-alert
@@ -13,6 +14,7 @@
     closable
     :closeText="`${customText}`"
     v-if="`${button}` === 'custom'"
+    :icon="infoIcon"
   />
   <a-alert
     :message="`${text}`"
@@ -20,10 +22,14 @@
     show-icon
     closable
     v-if="`${button}` === 'close'"
+    :icon="infoIcon"
   />
 </template>
 <script>
 import Banner from "ant-design-vue/lib/alert";
+import SystemInfo from "@meetinghand/style/icons/systemStatusInfo";
+
+import { h } from "vue";
 
 export default {
   components: {
@@ -49,7 +55,17 @@ export default {
       validator: (_v) => ["warning", "success"].includes(_v),
     },
   },
+  setup() {
+    const infoIcon = h(SystemInfo);
+    return {
+      infoIcon,
+    };
+  },
 };
 </script>
 <style scoped>
+svg {
+  fill: #000;
+  min-width: 20px;
+}
 </style>
