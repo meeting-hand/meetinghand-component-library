ecee3f363496d0ea00d8697232bf151b973da3c8
EmreKaanSurgun <emrekaansurgun@gmail.com>
2021-05-31 15:02:40 +0300

    variables added

--- packages/tooltip/tooltip.stories.js
+++ packages/tooltip/tooltip.stories.js
@@ -9,21 +9,25 @@ storiesOf("Tooltip", module).add("All", () => ({
   template: `
     <div class="tooltip-wrapper">
       <h1>Placement Top</h1>
+      <br/><br/>
       <tooltip size="large" placement="top" text="Lorem ipsum dolor sit amet.">
       Tooltip will show when mouse enter.
       </tooltip>
       <hr/>
       <h1>Placement Bottom</h1>
+      <br/><br/>
       <tooltip size="small" placement="bottom" text="Lorem ipsum dolor sit amet.">
       Tooltip will show when mouse enter.
       </tooltip>
       <hr/>
       <h1>Placement Right</h1>
+      <br/><br/>
       <tooltip size="small" placement="right" text="Lorem ipsum dolor sit amet.">
       Tooltip will show when mouse enter.
       </tooltip>
       <hr/>
       <h1>Placement Left</h1>
+      <br/><br/>
       <tooltip size="small" placement="left" text="Lorem ipsum dolor sit amet.">
       Tooltip will show when mouse enter.
       </tooltip>
