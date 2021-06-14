92b64df267fc9ca4a6188049c32461373e04f70f
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2021-06-14 11:45:19 +0300

    ant design scss files

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
 import "../packages/style/lib/scss/reset.scss";
 import "../packages/style/lib/scss/colors.scss";
 import "../packages/style/lib/scss/typography.scss";
-import "../packages/style/lib/less/ant.less";
+//import "../packages/style/lib/less/ant.less";
