aa2d69a4a5d45d6aeccf99af5cb5f42f2f746f76
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2021-06-15 12:59:48 +0300

    f

--- packages/textarea/src/index.vue
+++ packages/textarea/src/index.vue
@@ -19,7 +19,6 @@
 import { Input } from "ant-design-vue";
 import MHIcon from "@meetinghand/style/icons/index.vue";
 
-import "ant-design-vue/lib/input/style/index.less";
 import "./assets/main.scss";
 
 export default {
