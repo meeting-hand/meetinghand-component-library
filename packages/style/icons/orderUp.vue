82fe4ae9a21156eedcf2cfd145f020e0225f08f3
EmreKaanSurgun <emrekaansurgun@gmail.com>
2021-12-07 18:47:41 +0300

    f

--- packages/style/icons/orderUp.vue
+++ packages/style/icons/orderUp.vue
@@ -1,6 +1,18 @@
 <template>
   <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-    <path stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    <path
+      d="M7 8L10 5L13 8"
+      stroke="#979797"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M7 12L10 15L13 12"
+      stroke="#979797"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
   </svg>
 </template>
