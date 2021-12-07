95b022cdae256b426f284d816d82ec1899109330
EmreKaanSurgun <emrekaansurgun@gmail.com>
2021-12-07 18:37:04 +0300

    order up icon fix

--- packages/style/icons/orderUp.vue
+++ packages/style/icons/orderUp.vue
@@ -1,16 +1,6 @@
 <template>
   <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M13 12L10 15L7 12"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-    <path
-      d="M13 8L10 5L7 8"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
+    <path stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    <path stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
   </svg>
 </template>
