f471dee5dde7aac83f509bce3bdd415d16d406e9
EmreKaanSurgun <emrekaansurgun@gmail.com>
2021-12-09 15:53:41 +0300

    text preview fix

--- packages/submission-preview/src/types/textPreview.vue
+++ packages/submission-preview/src/types/textPreview.vue
@@ -37,13 +37,13 @@
         <div v-html="abstract.value"></div>
       </div>
     </div>
-    <div class="abstract-keywords">
+    <div class="abstract-keywords" v-if="keywords">
       <p>{{ labels.keywords }}:</p>
       <span v-for="(keyword, index) in keywords" :key="index">
         {{ keyword }}
       </span>
     </div>
-    <div class="abstract-references">
+    <div class="abstract-references" v-if="references">
       <h2 class="file-preview-title">{{ labels.references }}:</h2>
       <ul>
         <li v-for="(reference, index) in references" :key="index">
