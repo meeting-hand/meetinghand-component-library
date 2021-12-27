194fa9cb9601729c00c7e8c7bb0c47867b261c80
EmreKaanSurgun <emrekaansurgun@gmail.com>
2021-12-27 15:08:51 +0300

    tab bar button

---
+++ packages/button/src/types/tabBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <a :class="[{ active: isActive }, `tab-bar-btn`]">
+    <span>
+      <slot></slot>
+    </span>
+  </a>
+</template>
+<script>
+import { Button } from "ant-design-vue";
+import MHIcon from "@meetinghand/style/icons/index.vue";
+export default {
+  name: "MhButtonTabBar",
+  components: {
+    [Button.name]: Button,
+    "mh-icon": MHIcon,
+  },
+  props: {
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
