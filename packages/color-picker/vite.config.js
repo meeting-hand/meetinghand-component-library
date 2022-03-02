37070bd456c7bde014e86ae08875780f4b9546ec
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2022-03-02 18:28:58 +0300

    ant color pucker

--- packages/color-picker/vite.config.js
+++ packages/color-picker/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
             fileName: (format) => `lib.${format}.js`,
         },
         rollupOptions: {
-            //external: ["vue"],
+            external: ["vue"],
             output: {
                 // Provide global variables to use in the UMD build
                 // Add external deps here
