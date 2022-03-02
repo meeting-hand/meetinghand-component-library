ee42a2a289e197a7acdf7bcc63877fcd6581e772
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2022-03-02 13:53:19 +0300

    f

--- packages/banner/vite.config.js
+++ packages/banner/vite.config.js
@@ -1,25 +1,3 @@
-const path = require("path");
-const { defineConfig } = require("vite");
-const vue = require("@vitejs/plugin-vue");
+import { getClientConfiguration } from "../../configuration";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, "index.js"),
-            name: "MhBanner",
-            fileName: (format) => `lib.${format}.js`,
-        },
-        rollupOptions: {
-            external: ["vue"],
-            output: {
-                // Provide global variables to use in the UMD build
-                // Add external deps here
-                globals: {
-                    vue: "Vue",
-                },
-            },
-        },
-    },
-    plugins: [vue()],
-});
+export default getClientConfiguration();
