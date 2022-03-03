a8f3772a6d12cf2dca19e0a1fc50b011b580b489
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2022-03-03 09:45:15 +0300

    select

--- packages/select/src/types/default.vue
+++ packages/select/src/types/default.vue
@@ -30,7 +30,7 @@
 <script>
 import { computed, h } from "vue";
 
-import { Select } from "ant-design-vue";
+import Select from "ant-design-vue/es/select";
 import ArrowIcon from "@meetinghand/style/icons/chevronDown.vue";
 import SystemClose from "@meetinghand/style/icons/systemClose.vue";
 import StatusError from "@meetinghand/style/icons/systemStatusError.vue";
