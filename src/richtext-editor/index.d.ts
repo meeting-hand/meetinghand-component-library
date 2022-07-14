f5d5dd9dff67204bbc69f425718037ff8b728766
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2022-07-14 16:11:51 +0300

    tiny mce added

---
+++ src/richtext-editor/index.d.ts
@@ -0,0 +1,1 @@
+declare module "@meetinghand/lib/richtext-editor"
---
+++ stories/RichTextEditor.stories.js
@@ -0,0 +1,40 @@
+import { storiesOf } from "@storybook/vue3";
+import { defineAsyncComponent } from "vue";
+
+storiesOf("RichTextEditor", module).add("All", () => ({
+    components: {
+        MhTextEditor: defineAsyncComponent({
+            loader: () =>
+                import ("../src/richtext-editor/src/index.vue"),
+        }),
+    },
+    data() {
+        return {
+            text1: "<p><b>Gandalf</b> the grey</p>",
+            text2: "",
+            text3: "",
+            text4: "",
+            toolbar: "undo redo | casechange blocks | bold italic backcolor forecolor link image superscript subscript | \
+           alignleft aligncenter alignright alignjustify | \
+           bullist numlst outdent indent table charmap fullscreen | removeformat | a11ycheck code help",
+        };
+    },
+    template: `
+      <h1>Default</h1>
+	  		<mh-text-editor elementRef="text1" v-model="text1" placeholder="Placeholder" label="Lorem ipsum" />
+      	<br /><br />
+				{{ text1 }}
+
+			<h1>Default with word counter</h1>
+      	<mh-text-editor elementRef="text2" v-model="text2" placeholder="Placeholder" :toolbar="toolbar" :max-word-count="15" />
+      	<br /><br />
+      	<p>{{text2}}</p>
+      <hr />
+      <h1>Error</h1>
+      <mh-text-editor elementRef="text3" v-model="text3" placeholder="Placeholder" :toolbar="toolbar" :max-word-count="15" has-error error-message="this field is required"/>
+      <hr />
+      <h1>Disabled</h1>
+      <mh-text-editor elementRef="text4" v-model="text4" placeholder="Placeholder" :toolbar="toolbar" :readOnly="true"/>
+
+    `,
+}));
